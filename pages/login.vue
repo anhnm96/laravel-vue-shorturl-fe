5bc568faa026886a3c207a149dbb32dfb1122fdf
Nguyen Minh Anh <xiaosasori@gmail.com>
2024-04-02 17:39:10 +0900

    simplify form validation with formkit

--- pages/login.vue
+++ pages/login.vue
@@ -1,50 +1,27 @@
 <script setup lang="ts">
+import type { LoginPayload } from '@/types'
+
 definePageMeta({
   layout: "centered",
   middleware: ["guest"],
-});
+})
 
 const { login } = useAuth()
 
-const form = ref({
-  email: '',
-  password: ''
-})
-
-const errors = ref({
-  email: [],
-  password: []
-})
-
-async function handleLogin() {
-  const { error } = await login(form.value)
+async function handleLogin(payload: LoginPayload, node?: any) {
+  const { error } = await login(payload)
   if (error.value?.statusCode === 422) {
-    console.log('asd das', JSON.stringify(error.value))
-    errors.value = error.value.data.errors
+    node?.setErrors([], error.value.data.errors)
   }
 }
 </script>
 <template>
   <div class="login" @submit.prevent="handleLogin">
     <h1>Login</h1>
-    <form>
-      <label>
-        <div>Email</div>
-        <input type="text" v-model="form.email" />
-        <div class="text-red-600 p-1" v-for="error in errors.email">
-          {{ error }}
-        </div>
-      </label>
-
-      <label>
-        <div>Password</div>
-        <input type="password" v-model="form.password" />
-        <div class="text-red-600 p-1" v-for="error in errors.password">
-          {{ error }}
-        </div>
-      </label>
-      <button class="btn">Login</button>
-    </form>
+    <FormKit type="form" submit-label="Login" @submit="handleLogin">
+      <FormKit label="Email" name="email" type="email" />
+      <FormKit label="Password" name="password" type="password" />
+    </FormKit>
 
     <p>
       Don't have an account?
